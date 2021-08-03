49a73357d1a980c87674dc78eb7545546ce5a998
Florian Wagner <florian.wagner@microsoft.com>
2021-08-03 02:36:00 +0000

    Test CI
    Signed-off-by: Florian Wagner <florian.wagner@microsoft.com>

--- super-rentals/app/routes/rental.js
+++ super-rentals/app/routes/rental.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 export default class RentalRoute extends Route {
   @service store;
 
-  async model() {
+  async model(params) {
     return this.store.findRecord('rental', params.rental_id);
   }
 }
